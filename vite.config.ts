c815d1551973dadadeb23d0b65a40251a79d869d
Michael Maio <MichaelMaio@users.noreply.github.com>
2025-07-29 15:14:08 -0700

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/VendingMachine/', // this fixes path resolution on GitHub Pages
   plugins: [react()],
 });
