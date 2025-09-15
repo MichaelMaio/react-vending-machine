c391ee6e86834bcc8893ee690848a569883ca0ea
Michael Maio <32399603+MichaelMaio@users.noreply.github.com>
2025-09-15 13:56:01 -0700

    Update vite.config.ts to rename repo to react-vending-machine.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/web-vending-machine/', // this fixes path resolution on GitHub Pages
+  base: '/react-vending-machine/', // this fixes path resolution on GitHub Pages
   plugins: [react()],
 });
